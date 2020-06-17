4038ccb23b8e16b37e8868dc37c6c83f7e81fa7c
Ana Berrocal <anaberrocalmail@gmail.com>
2020-06-17 16:47:19 -0400

    Updates

---
+++ precache-manifest.45b284aa826fae9e261685a429984d3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53e0331608d83a26521884b02ef52e2",
+    "url": "/crash-tracker/index.html"
+  },
+  {
+    "revision": "6ecfd27f34ace6bed212",
+    "url": "/crash-tracker/static/css/main.bf73dd89.chunk.css"
+  },
+  {
+    "revision": "a7adfa7f93438e7df345",
+    "url": "/crash-tracker/static/js/2.1ed6db72.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/crash-tracker/static/js/2.1ed6db72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ecfd27f34ace6bed212",
+    "url": "/crash-tracker/static/js/main.602e2ff7.chunk.js"
+  },
+  {
+    "revision": "b2c170f15c6615e07dd8",
+    "url": "/crash-tracker/static/js/runtime-main.26ffcdea.js"
+  },
+  {
+    "revision": "f28522bd3b98706f79a08effae1d27b1",
+    "url": "/crash-tracker/static/media/crashIconHigh.f28522bd.png"
+  },
+  {
+    "revision": "5e53030df199c93259a20f14f71c18a3",
+    "url": "/crash-tracker/static/media/crashIconLow.5e53030d.png"
+  },
+  {
+    "revision": "9d350d49246f0d16d1c097dcdf4dd2b5",
+    "url": "/crash-tracker/static/media/crashTracker.9d350d49.png"
+  }
+]);
